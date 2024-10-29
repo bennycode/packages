9eb92c67e5ef2c54a81c4eeb899b8ca22bc0c525
Benny Neugebauer <mail@bennycode.com>
2024-10-29 12:05:42 +0100

    test: Add tests for utils

---
+++ packages/utils/README.md
@@ -0,0 +1,3 @@
+## @bennycode/utils
+
+This package includes everyday utilities to simplify file handling in Node.js. Developed and used by [@bennycode](https://bennycode.com/).
--- packages/utils/src/createTempFile.ts
+++ packages/utils/src/createTempFile.ts
@@ -6,25 +6,30 @@ import path from "path";
 /**
  * Creates a temporary file that is automatically deleted when exiting the application.
  * @param content - Temporary content to write into the file
+ * @param cleanup - Wether the temp file should be deleted on application exit
  * @returns The file path
  */
-export async function createTempFile(content: string | undefined | null) {
+export function createTempFile(
+  content: string = "",
+  cleanup: boolean = true
+): string {
   const uuid = crypto.randomUUID();
   const tempDir = fs.mkdtempSync(path.join(os.tmpdir(), `${uuid}-`));
   const tempFilePath = path.join(tempDir, `${uuid}.txt`);
 
-  fs.writeFileSync(tempFilePath, content || "");
+  fs.writeFileSync(tempFilePath, content);
   console.debug(`Temporary file created at: ${tempFilePath}`);
 
-  const deleteFile = () => {
-    fs.unlinkSync(tempFilePath);
-    fs.rmdirSync(tempDir);
-  };
+  if (cleanup) {
+    const deleteFile = () => {
+      fs.rmSync(tempDir, { force: true, recursive: true });
+    };
 
-  // Delete temporary file when program exits
-  process.once("exit", deleteFile);
-  process.once("SIGINT", deleteFile);
-  process.once("SIGTERM", deleteFile);
+    // Delete temporary file when program exits
+    process.once("exit", deleteFile);
+    process.once("SIGINT", deleteFile);
+    process.once("SIGTERM", deleteFile);
+  }
 
   return tempFilePath;
 }
